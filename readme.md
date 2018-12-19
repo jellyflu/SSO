a50ba05689eec7586d4eb95a459e9ad32957f8b0
jellyflu <exkuarbel@163.com>
2018-12-20 01:21:49 +0800

    1

--- readme.md
+++ readme.md
@@ -1,1 +1,69 @@
 # SSO
+java实现的SSO案例(springboot)，有SSO后台管理界面，SSO全局、局部会话保存到mysql及redis库中。
+
+
+
+# 工程介绍
+
+##  sso-base  
+
+ SSO项目基础工程，封装了一些sso-server、sso-client都需要的模型类、工具类、redis辅助bean等
+
+## sso-server:
+
+ SSO服务端工程(http://www.mysso.com:9001) 
+
+1、提供了供sso-client验证用户是否登录、sso用户登录与注销等接口
+
+2、提供了SSO后台管理页面(后台人员使用)
+
+## sso-site1
+
+ SSO客户端工程(站点1,http://www.site1.com:8001)
+
+## sso-site2
+
+ SSO客户端工程(站点2,http://www.site2.com:8002)
+
+
+
+# 环境准备
+
+配置本机hosts文件，以方便测试SSO登录
+
+127.0.0.1  www.site1.com
+127.0.0.1  www.site2.com
+127.0.0.1  www.mysso.com
+
+
+
+# 运行截图
+
+sso用户登录页面:
+![Image text](https://raw.githubusercontent.com/jellyflu/pub_resouces/master/SSO/ssoLogin.png)
+
+sso后台管理页面1:
+![Image text](https://raw.githubusercontent.com/jellyflu/pub_resouces/master/SSO/ssoAdmin-1.png)
+
+sso后台管理页面2:
+![Image text](https://raw.githubusercontent.com/jellyflu/pub_resouces/master/SSO/ssoAdmin-2.png)
+
+
+site1 订单页面1:
+![Image text](https://raw.githubusercontent.com/jellyflu/pub_resouces/master/SSO/1.png)
+
+
+site1 订单页面2: 
+![Image text](https://raw.githubusercontent.com/jellyflu/pub_resouces/master/SSO/2.png)
+
+
+site2 订单页面1:
+![Image text](https://raw.githubusercontent.com/jellyflu/pub_resouces/master/SSO/3.png)
+
+
+site2 订单页面2:
+![Image text](https://raw.githubusercontent.com/jellyflu/pub_resouces/master/SSO/4.png) 
+
+#  其他说明
+
+sso-client :  sso的客户端插件，有空再整出来，可以单独抽取出来，封装成jar。当某站点想要接入SSO单点登录系统时，只需要引入这个客户端插件jar包，并进行相应配置即可。
---
+++ sso-server/src/main/resources/sso.sql
@@ -0,0 +1,63 @@
+/*
+SQLyog Ultimate v12.5.0 (64 bit)
+MySQL - 5.7.16 : Database - sso
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sso` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `sso`;
+
+/*Table structure for table `t_sessioninfo` */
+
+DROP TABLE IF EXISTS `t_sessioninfo`;
+
+CREATE TABLE `t_sessioninfo` (
+  `sessionId` varchar(50) NOT NULL COMMENT '会话id',
+  `sessionType` smallint(6) DEFAULT NULL COMMENT '会话类型 1全局SSO会话 2局部site会话',
+  `parentSessionId` varchar(50) DEFAULT NULL COMMENT '父会话id',
+  `domain` varchar(256) DEFAULT NULL COMMENT '站点域,如www.site1.com',
+  `browserIp` varchar(32) DEFAULT NULL COMMENT '浏览器ip',
+  `browserName` varchar(32) DEFAULT NULL COMMENT '浏览器名称',
+  `loginTime` varchar(24) DEFAULT NULL COMMENT '登录时间',
+  `logoutTime` varchar(24) DEFAULT NULL COMMENT '退出时间',
+  `isOnline` smallint(6) DEFAULT '1' COMMENT '是否在线 1在线 0离线',
+  `userId` varchar(32) DEFAULT NULL COMMENT '关联用户id',
+  `username` varchar(32) DEFAULT NULL COMMENT '关联用户名',
+  PRIMARY KEY (`sessionId`),
+  KEY `parentSessionId` (`parentSessionId`),
+  CONSTRAINT `t_sessioninfo_ibfk_1` FOREIGN KEY (`parentSessionId`) REFERENCES `t_sessioninfo` (`sessionId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ 
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `userId` varchar(32) NOT NULL COMMENT '主键id uuid',
+  `username` varchar(32) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(32) DEFAULT NULL COMMENT '密码',
+  `birthday` varchar(24) DEFAULT NULL COMMENT '生日',
+  `city` varchar(32) DEFAULT NULL COMMENT '城市',
+  `email` varchar(32) DEFAULT NULL COMMENT '邮箱',
+  `createtime` varchar(24) DEFAULT NULL COMMENT '创建时间',
+  `remark` varchar(200) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ 
+
+ 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
